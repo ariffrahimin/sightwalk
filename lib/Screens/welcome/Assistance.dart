96bae98a7a965d7766b5125a3f125f7adc28b1d7
Thitthima Tina <thitthima.p@gmail.com>
2020-12-18 13:11:44 +0800

    Adding chatbot

---
+++ lib/Screens/welcome/Assistance.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dialogflow/dialogflow_v2.dart';
+
+class Chatbot extends StatefulWidget {
+  Chatbot({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _ChatbotState createState() => new _ChatbotState();
+}
+class _ChatbotState extends State<Chatbot> {
+  final List<FactsMessage> _messages = <FactsMessage>[];
+
+  final TextEditingController _textController = new TextEditingController();
+
+  Widget _queryInputWidget(BuildContext context) {
+    return Container(
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onSubmitted: _submitQuery,
+                decoration: InputDecoration.collapsed(hintText: "Send a message"),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: () => _submitQuery(_textController.text)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  void _dialogFlowResponse(query) async {
+    _textController.clear();
+    AuthGoogle authGoogle =
+    await AuthGoogle(fileJson: "assets/sightwalk.json").build();
+    Dialogflow dialogFlow =
+    Dialogflow(authGoogle: authGoogle, language: Language.english);
+    AIResponse response = await dialogFlow.detectIntent(query);
+    FactsMessage message = FactsMessage(
+      text: response.getMessage() ??
+          CardDialogflow(response.getListMessage()[0]).title,
+      name: "Flutter Bot",
+      type: false,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+  void _submitQuery(String text) {
+    _textController.clear();
+    FactsMessage message = new FactsMessage(
+      text: text,
+      name: " ",
+      type: true,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+    _dialogFlowResponse(text);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("AI Assistant"),
+      ),
+      body: Column(children: <Widget>[
+        Flexible(
+            child: ListView.builder(
+              padding: EdgeInsets.all(8.0),
+              reverse: true, //To keep the latest messages at the bottom
+              itemBuilder: (_, int index) => _messages[index],
+              itemCount: _messages.length,
+            )),
+        Divider(height: 1.0),
+        Container(
+          decoration: new BoxDecoration(color: Theme.of(context).cardColor),
+          child: _queryInputWidget(context),
+        ),
+      ]),
+    );
+  }
+}
+class FactsMessage extends StatelessWidget {
+  FactsMessage({this.text, this.name, this.type});
+
+  final String text;
+  final String name;
+  final bool type;
+
+  List<Widget> userMessage(context) {
+    return <Widget>[
+      Expanded(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            Text(this.name, style: Theme.of(context).textTheme.headline6),
+            Container(
+              margin: const EdgeInsets.only(top: 5.0),
+              child: Text(text),
+            ),
+          ],
+        ),
+      ),
+      Container(
+        margin: const EdgeInsets.only(left: 16.0),
+        child: CircleAvatar(child: new Text(this.name[0])),
+      ),
+    ];
+  }
+  List<Widget> botMessage(context) {
+    return <Widget>[
+      Container(
+        margin: const EdgeInsets.only(right: 16.0),
+        child: CircleAvatar(child: Text('Bot')),
+      ),
+      Expanded(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(this.name,
+                style: TextStyle(fontWeight: FontWeight.bold)),
+            Container(
+              margin: const EdgeInsets.only(top: 5.0),
+              child: Text(text),
+            ),
+          ],
+        ),
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: this.type ? userMessage(context) : botMessage(context),
+      ),
+    );
+  }
+
+}
